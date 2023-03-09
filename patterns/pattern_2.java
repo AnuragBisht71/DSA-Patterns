2d1c73876c95031de5a464ec939a010298a34702
uchiha0007 <bishtanurag71@gmail.com>
2023-03-09 23:25:13 +0530

    solved pattern-2 question

---
+++ patterns/pattern_2.java
@@ -0,0 +1,27 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+}
+
+// Expected Output :-
+
+// *       *       *       *       *
+// *       *       *       *
+// *       *       *
+// *       *
+// *
