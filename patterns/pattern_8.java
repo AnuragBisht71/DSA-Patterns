657ab229a071d5cd6b7bbb519c1bef566be3d71a
uchiha0007 <bishtanurag71@gmail.com>
2023-03-12 13:09:50 +0530

    solved pattern-8 question

---
+++ patterns/pattern_8.java
@@ -0,0 +1,36 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_8 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            
+            for(int j = 1; j <= n; j++) {
+                
+                if(i + j == n + 1) {
+                    System.out.print("*\t");
+                }
+                else {
+                    System.out.print("\t");
+                }
+            }
+            
+            System.out.println();
+        }
+        scn.close();
+    }
+}
+
+// Expected Output :-
+
+//                    *
+//               *
+//          *
+//     *
+// *    
