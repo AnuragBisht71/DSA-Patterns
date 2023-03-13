dac670f4c2438730a33e1305f597b2b1665caaa6
uchiha0007 <bishtanurag71@gmail.com>
2023-03-13 13:28:53 +0530

    format for pattern-10 question

---
+++ patterns/pattern_10.java
@@ -0,0 +1,28 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+
+
+
+        scn.close();
+    }
+}
+
+// Expected Output :-
+
+//             *
+//            
+//       *           * 
+//     
+// *                       *
+//
+//       *           *
+//      
+//             *            
