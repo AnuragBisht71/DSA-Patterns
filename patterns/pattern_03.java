ecb33dd19a033a7fa45d0d79a47322f10d35d859
uchiha0007 <bishtanurag71@gmail.com>
2023-03-13 20:49:27 +0530

    solved pattern-3 question

--- patterns/pattern_03.java
+++ patterns/pattern_03.java
@@ -2,7 +2,7 @@ package patterns;
 
 import java.util.Scanner;
 
-public class pattern_3 {
+public class pattern_03 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter a number");
