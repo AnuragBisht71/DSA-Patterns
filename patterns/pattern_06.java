68468c95509ee37fdf68bfdca910f192ef276ea7
uchiha0007 <bishtanurag71@gmail.com>
2023-03-13 20:52:06 +0530

    solved pattern-6 question

--- patterns/pattern_06.java
+++ patterns/pattern_06.java
@@ -2,7 +2,7 @@ package patterns;
 
 import java.util.Scanner;
 
-public class pattern_6 {
+public class pattern_06 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter a number");
