5315f449f6fb7b54f3f63d9a36451c914a9360e7
uchiha0007 <bishtanurag71@gmail.com>
2023-03-13 20:53:18 +0530

    solved pattern-8 question

--- patterns/pattern_08.java
+++ patterns/pattern_08.java
@@ -2,7 +2,7 @@ package patterns;
 
 import java.util.Scanner;
 
-public class pattern_8 {
+public class pattern_08 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter a number");
