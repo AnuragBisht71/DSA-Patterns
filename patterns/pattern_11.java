d6081be6c3694dcbf17725c4087989993d833d21
uchiha0007 <bishtanurag71@gmail.com>
2023-03-15 11:32:42 +0530

    solved pattern-11 question

--- patterns/pattern_11.java
+++ patterns/pattern_11.java
@@ -20,6 +20,17 @@ public class pattern_11 {
 
         int n = scn.nextInt();
 
+        int val = 1;
+        for(int i = 1; i <= n; i++) {
+            
+            for(int j = 1; j <= i; j++) {
+                System.out.print(val + " ");
+                val++;
+            }
+
+            System.out.println();
+        }
+
         scn.close();
     }
 }
