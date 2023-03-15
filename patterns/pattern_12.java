96c8b6226d67179dcc69800d68550e8c382d39e3
uchiha0007 <bishtanurag71@gmail.com>
2023-03-15 11:33:59 +0530

    integrated code format

---
+++ patterns/pattern_12.java
@@ -0,0 +1,25 @@
+// Expected Output :-
+
+//  0
+//  1     1
+//  2     3     5
+//  8     13    21     34
+//  55    89    144    233    377
+
+
+// Code:-
+
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_12 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+
+        scn.close();
+    }
+}
