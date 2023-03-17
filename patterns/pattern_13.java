868df605cd92e1665772e9c504ce1551a9193caf
uchiha0007 <bishtanurag71@gmail.com>
2023-03-17 13:38:49 +0530

    solved pattern-13.java

--- patterns/pattern_13.java
+++ patterns/pattern_13.java
@@ -21,6 +21,19 @@ public class pattern_13 {
 
         int n = scn.nextInt();
 
+        for(int i = 0; i < n; i++) {
+            
+            int icj = 1;
+            for(int j = 0; j <= i; j++) {
+                System.out.print(icj + "\t");
+
+                int icjp1 = icj * (i-j) / (j+1);
+                icj = icjp1;
+            }
+
+            System.out.println();
+        }
+
         scn.close();
     }
 }
