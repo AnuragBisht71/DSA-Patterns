16c7cd1fae41ccb53df179cc19b16a6c25c0e4c2
uchiha0007 <bishtanurag71@gmail.com>
2023-03-17 13:39:22 +0530

    integrated code format

---
+++ patterns/pattern_14.java
@@ -0,0 +1,28 @@
+// Expected Output :-
+
+// 3 * 1 = 3
+// 3 * 2 = 6
+// 3 * 3 = 9
+// .
+// .
+// .
+// .
+// 3 * 10 = 30
+
+
+// Code:-
+
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_14 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+
+        scn.close();
+    }
+}
