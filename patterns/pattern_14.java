5144c294ac7e5f2e547aa1c7ba1dbb7dfd347458
uchiha0007 <bishtanurag71@gmail.com>
2023-03-18 10:29:44 +0530

    solved pattern-14 question

--- patterns/pattern_14.java
+++ patterns/pattern_14.java
@@ -21,7 +21,13 @@ public class pattern_14 {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter a number");
 
-        int n = scn.nextInt();
+        int x = scn.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+
+            int val = x * i;
+            System.out.println(x + " * " + i + " = " + val);
+        }
 
         scn.close();
     }
