0836ec77e6bdadd04115be3e7b9b4f1362bdb0d0
uchiha0007 <bishtanurag71@gmail.com>
2023-03-21 11:17:17 +0530

    integrated code format

---
+++ patterns/pattern_19.java
@@ -0,0 +1,29 @@
+// Expected Output:-
+
+// *    *    *         *
+
+//           *         *
+
+// *    *    *    *    *
+
+// *         *
+
+// *         *    *    *
+
+
+// Code:-
+
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_19 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+
+        scn.close();
+    }
+}
