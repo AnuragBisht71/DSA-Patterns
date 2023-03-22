87b6e89820bbb657a3e287c1afc6752501bb2658
uchiha0007 <bishtanurag71@gmail.com>
2023-03-22 10:25:14 +0530

    solved pattern-19 question

--- patterns/pattern_19.java
+++ patterns/pattern_19.java
@@ -24,6 +24,54 @@ public class pattern_19 {
 
         int n = scn.nextInt();
 
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= n; j++) {
+
+                if(i == 1) {
+                    if(j == n || j <= n/2+1) {
+                        System.out.print("*\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+                
+                else if(i <= n/2) {
+                    if(j == n || j == n/2+1) {
+                        System.out.print("*\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+
+                else if(i == n/2+1) {
+                    System.out.print("*\t");
+                }
+                
+                else if(i < n) {
+                    if(j == 1 || j == n/2+1) {
+                        System.out.print("*\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+                
+                else {
+                    if(j == 1 || j >= n/2+1) {
+                        System.out.print("*\t");
+                    }
+                    else {
+                        System.out.print("\t");
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+
         scn.close();
     }
 }
