a487ec1c17b641f018698c20d76057a45a404013
uchiha0007 <bishtanurag71@gmail.com>
2023-03-22 10:26:42 +0530

    integrated code format

---
+++ patterns/pattern_20.java
@@ -0,0 +1,25 @@
+// Expected Output:-
+
+// *                   *
+// *                   *
+// *         *         *
+// *    *         *    *
+// *                   *
+
+
+// Code:-
+
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern_20 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = scn.nextInt();
+
+        scn.close();
+    }
+}
