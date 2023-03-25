b7f594b1133966decf6203594142937b64db541a
uchiha0007 <bishtanurag71@gmail.com>
2023-03-25 09:25:05 +0530

    integrated question

---
+++ functions/function_5.java
@@ -0,0 +1,37 @@
+// Any base addition
+
+// Input :-
+
+// 8 --> base
+// 777 --> n1 
+// 1 --> n2
+
+// Output :-
+
+// 1000
+
+
+package functions;
+
+import java.util.Scanner;
+
+public class function_5 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int b = scn.nextInt();
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+    
+        int d = getSum(b, n1, n2);
+        System.out.println(d);
+
+        scn.close();
+    }
+
+    public static int getSum(int b, int n1, int n2) {
+        return n2;
+
+    }
+}
---
+++ functions/function_6.java
@@ -0,0 +1,36 @@
+// Any base subtraction
+
+// Input :-
+
+// 8 --> base
+// 1 --> n1 
+// 100 --> n2
+
+// Output :-
+
+// 77
+
+package functions;
+
+import java.util.Scanner;
+
+public class function_6 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int b = scn.nextInt();
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+    
+        int d = getDifference(b, n1, n2);
+        System.out.println(d);
+
+        scn.close();
+    }
+
+    public static int getDifference(int b, int n1, int n2) {
+        return n2;
+        
+    }
+}
