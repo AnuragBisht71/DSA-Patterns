8854d8b58e565d0406cafe0f714f9c2f88cb8d56
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-26 12:19:53 +0530

    solved function question

--- functions/function_6.java
+++ functions/function_6.java
@@ -30,6 +30,30 @@ public class function_6 {
     }
 
     public static int getDifference(int b, int n1, int n2) {
-        return n2;
+        int res = 0;
+        int pos = 1;
+        int borrow = 0;
+
+        while(n1 != 0 || n2 != 0) {
+            int d1 = n1 % 10;
+            int d2 = n2 % 10;
+
+            int diff = d2 - d1 - borrow;
+            if(diff < 0) {
+                diff = diff + b;
+                borrow = 1;
+            }
+            else {
+                borrow = 0;
+            }
+
+            res = res + (diff * pos);
+            n1 = n1 / 10;
+            n2 = n2 / 10;
+            pos = pos * 10;
+        }
+
+        System.out.println("Output :-");
+        return res;
     }
 }
