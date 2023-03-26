fa1af939923f224c24367e7e264495cb18303fa9
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-26 12:27:35 +0530

    integrated array question

---
+++ arrays/array_01.java
@@ -0,0 +1,31 @@
+// Find element in an array
+
+// Input :-
+// 6 --> no. of elements
+// 15
+// 30
+// 40
+// 4
+// 11
+// 9
+// 40 --> find
+
+// Output :-
+
+// 2
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_01 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        
+
+        scn.close();
+    }
+}
