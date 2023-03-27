6dcf4ba8fcef695cc5280f16144c530bcaf5198a
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-27 10:35:47 +0530

    solved array-1 question

--- arrays/array_01.java
+++ arrays/array_01.java
@@ -24,8 +24,28 @@ public class array_01 {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter input :-");
 
-        
+        int n = scn.nextInt();
+
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int k = scn.nextInt();
+        int res = find(arr,k);
+        System.out.println(res);
 
         scn.close();
+    }
+
+    public static int find(int[] arr, int k) {
+        for(int idx = 0; idx < arr.length; idx++) {
+            if(arr[idx] == k) {
+                System.out.println("Output :-");
+                return idx;
+            }
+        }
+
+        return -1;
     }
 }
