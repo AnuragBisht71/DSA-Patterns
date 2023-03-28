ae3237af93b0be47b2c862894f3612c4b93710d1
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-28 13:40:16 +0530

    interated code format

---
+++ arrays/array_05.java
@@ -0,0 +1,32 @@
+// Sum of two arrays
+
+// Input :-
+
+// 5 --> no. of elements
+// arr 1 = [3,1,0,7,5];
+
+// 6 --> no. of elements
+// arr 2 = [1,1,1,1,1,1];
+
+// Output :-
+
+// 1
+// 4
+// 2
+// 1
+// 8
+// 6
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_05 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        scn.close();
+    }
+}
