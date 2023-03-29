232ee989bb334dbf37c1b770b756e864812e2946
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-29 13:18:11 +0530

    integrated question

---
+++ arrays/array_06.java
@@ -0,0 +1,28 @@
+// Difference of two array
+
+// Input :-
+// 3 --> no. of elements
+// arr1 = [2, 6, 7]
+
+// 4 --> no. of elements
+// arr2 = [1, 0, 0, 0];
+
+// Output :-
+
+// 7
+// 3
+// 3
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_06 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        scn.close();
+    }
+}
