93ddd10ea7eec3f71cb9665e39336eff097972f3
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-30 11:08:55 +0530

    integrated code format

---
+++ arrays/array_08.java
@@ -0,0 +1,36 @@
+// Bar chart
+
+// Input :-
+
+// 5
+// 3
+// 1
+// 0
+// 7
+// 5
+
+// Output :-
+
+//                *
+//                *
+//                *    *
+//                *    *
+// *              *    *
+// *              *    *
+// *    *         *    *
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_08 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int n = scn.nextInt();
+
+        scn.close();
+    }
+}
