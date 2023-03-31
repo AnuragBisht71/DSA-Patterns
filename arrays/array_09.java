f09c4cc7c652c44954a204f23d3b4b9ff2a1de8b
AnuragBisht71 <bishtanurag71@gmail.com>
2023-03-31 10:09:29 +0530

    integrated question

---
+++ arrays/array_09.java
@@ -0,0 +1,29 @@
+// Broken economy
+
+// Input :-
+
+// 10 --> no. of elements
+// a = {1 , 5 , 10 , 15 , 22 , 33 , 40 , 42 , 55 , 66 };
+// 34 --> d(required to find the ceil and floor of d in array a)
+
+// Output :-
+
+// 40 
+// 33
+
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_09 {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter input :-");
+
+    int n = scn.nextInt();
+
+    scn.close();
+    }
+}
