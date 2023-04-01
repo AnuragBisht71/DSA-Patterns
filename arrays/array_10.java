67e8c21932badadef06194fd9fe31fe2d467023c
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-01 21:04:43 +0530

    integrated question

---
+++ arrays/array_10.java
@@ -0,0 +1,27 @@
+// First Index and Last Index
+
+// Input :-
+
+// 15 --> no. of elements
+// arr = { 1 , 5 , 10 , 15 , 22 , 33 , 33 , 33 , 33 , 33 , 40 , 42 , 55 , 66 , 77 }
+// 33 --> find the first and last index
+
+// Output :-
+
+// 5
+// 9
+
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        scn.close();
+    }
+}
