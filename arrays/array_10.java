6a6a930be9e41f168c2b0ab46af8329dc26cef22
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-02 10:23:49 +0530

    solved array-10 question

--- arrays/array_10.java
+++ arrays/array_10.java
@@ -22,6 +22,64 @@ public class array_10 {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter input :-");
 
+        int n = scn.nextInt();
+
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int k = scn.nextInt();
+        System.out.println("Output :-");
+        System.out.println(firstIndex(arr,k));
+        System.out.println(lastIndex(arr,k));
+
         scn.close();
+    }
+
+    public static int firstIndex(int[] arr, int k) {
+        int lo = 0;
+        int hi = arr.length-1;
+        int fi = -1;
+
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if(k == arr[mid]) {
+                fi = mid;
+                hi = mid - 1;
+            }
+            else if(k > arr[mid]) {
+                lo = mid + 1;
+            }
+            else if(k < arr[mid]) {
+                hi = mid - 1;
+            }
+        }
+
+        return fi;
+    }
+    
+    public static int lastIndex(int[] arr, int k) {
+        int lo = 0;
+        int hi = arr.length-1;
+        int li = -1;
+
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if(k == arr[mid]) {
+                li = mid;
+                lo = mid + 1;
+            }
+            else if(k > arr[mid]) {
+                lo = mid + 1;
+            }
+            else if(k < arr[mid]) {
+                hi = mid - 1;
+            }
+        }
+
+        return li;
     }
 }
