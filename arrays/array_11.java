c5d35c38786a86adac3da70fc3e90e9770dfe1e8
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-02 10:27:18 +0530

    added question

---
+++ arrays/array_11.java
@@ -0,0 +1,34 @@
+// Subarray Problem
+
+// Input :-
+
+// 3
+// 10
+// 20
+// 30
+
+// Output :=
+
+// 10	
+// 10	20	
+// 10	20	30	
+// 20	
+// 20	30	
+// 30	
+
+
+
+package arrays;
+
+import java.util.Scanner;
+
+public class array_11 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int n = scn.nextInt();
+
+        scn.close();
+    }
+}
