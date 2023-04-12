2b2bd4e7309cff4a65f714961af029ed99e8f39f
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-12 06:27:30 +0530

    solved 2d array-8 question

--- 2d arrays/array_08.java
+++ 2d arrays/array_08.java
@@ -22,18 +22,39 @@ import java.util.Scanner;
 
 public class array_08 {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter input :-");
+        try (Scanner scn = new Scanner(System.in)) {
+            System.out.println("Enter input :-");
 
-        int n = scn.nextInt();
-        int mat[][] = new int[n][n];
-        for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < n ; j++) {
-                mat[i][j] = scn.nextInt();
+            int n = scn.nextInt();
+            int mat[][] = new int[n][n];
+            for(int i = 0 ; i < n ; i++) {
+                for(int j = 0 ; j < n ; j++) {
+                    mat[i][j] = scn.nextInt();
+                }
             }
+
+            int x = scn.nextInt();
+            int i = 0;
+            int j = n-1;
+
+            while(j >= 0 && i < n) {
+                if(mat[i][j] == x) {
+                    System.out.println("Output :-");
+                    System.out.println(i);
+                    System.out.println(j);
+                    return;
+                }
+                else if(x > mat[i][j]) {
+                    i++;
+                }
+                else if(x < mat[i][j]) {
+                    j--;
+                }
+            }
+
+            System.out.println("Not Found");
+
+            scn.close();
         }
-
-
-        scn.close();
     }
 }
