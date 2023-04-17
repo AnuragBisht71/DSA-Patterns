6c5140ab8665105d04a67ea50e4149687f3c61c5
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-17 09:45:48 +0530

    solved string-5 question

---
+++ strings_stringbuilder_arraylist/strings/strings/strings_stringbuilder/string_05.java
@@ -0,0 +1,56 @@
+// Remove Primes
+
+// Input :-
+
+// 4 --> no. of elements
+// 3 12 13 15
+
+// Output :-
+
+// [12, 15]
+
+
+
+package strings_stringbuilder_arraylist.strings.strings.strings_stringbuilder;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class string_05 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int n = scn.nextInt();
+        
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i = 0 ; i < n; i++){
+            al.add(scn.nextInt());
+        }
+        
+        solution(al);
+        System.out.println("Output :-");
+        System.out.println(al);
+
+        scn.close();
+    }
+    
+    public static boolean isPrime(int val) {
+        for(int div = 2 ; div * div <= val ; div++) {
+            if(val % div == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void solution(ArrayList<Integer> al){
+        for(int idx = al.size()-1 ; idx >= 0 ; idx--) {
+            int val = al.get(idx);
+
+            if(isPrime(val)) {
+                al.remove(idx);
+            }
+        }
+    }
+}
