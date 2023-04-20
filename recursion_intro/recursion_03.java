17c0da17be62375bb0f67aaafa45d910b22e13b3
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-20 10:06:15 +0530

    solved recursion-3 question

---
+++ recursion_intro/recursion_03.java
@@ -0,0 +1,44 @@
+// Print Increasing Decreasing
+
+// Input :-
+
+// 3
+
+// Output :-
+
+// 3
+// 2
+// 1
+// 1
+// 2
+// 3
+
+
+
+package recursion_intro;
+
+import java.util.Scanner;
+
+public class recursion_03 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int n = scn.nextInt();
+
+        System.out.println("Output :-");
+        pdi(n);
+
+        scn.close();
+    }
+
+    public static void pdi(int n){
+        if(n == 0) {
+            return;
+        }
+        
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+}
