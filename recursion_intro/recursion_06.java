3f686f7f79812022ea8a9043c37ffc4f8c03ec36
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-22 17:41:09 +0530

    solved recursion-6 question

---
+++ recursion_intro/recursion_06.java
@@ -0,0 +1,45 @@
+// Power-logarithmic
+
+// Input :-
+
+// 2
+// 5
+
+// Output :-
+
+// 32
+
+
+
+package recursion_intro;
+
+import java.util.Scanner;
+
+public class recursion_06 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+
+        System.out.println("Output :-");
+        System.out.println(power(x,n));
+
+        scn.close();
+    }
+
+    public static int power(int x, int n){
+        if(n == 0) {
+            return 1;
+        }
+
+        int xPowNm1 = power(x, n / 2);
+        int xPowN = xPowNm1 * xPowNm1;
+        if(n%2 == 1) {
+            xPowN *= x;
+        }
+
+        return xPowN;
+    }
+}
