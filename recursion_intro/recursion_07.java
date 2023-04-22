dbdf5ad9ad89afa2fd4f68f755f4d48ac332d226
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-22 17:44:04 +0530

    added code format

---
+++ recursion_intro/recursion_07.java
@@ -0,0 +1,33 @@
+// Print Zigzag
+
+// Input :-
+
+// 3
+
+// Output :-
+
+// 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+
+
+
+package recursion_intro;
+
+import java.util.Scanner;
+
+public class recursion_07 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter input :-");
+
+        int n = scn.nextInt();
+
+        System.out.println("Output :-");
+        pzz(n);
+
+        scn.close();
+    }
+
+    public static void pzz(int n){
+        
+    }
+}
