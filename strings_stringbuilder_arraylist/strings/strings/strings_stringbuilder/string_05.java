1fabe76f119d47c5854c4ee9e8ef8424816a0b74
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-30 13:24:58 +0530

    solved string-5 question

--- strings_stringbuilder_arraylist/strings/strings/strings_stringbuilder/string_05.java
+++ strings_stringbuilder_arraylist/strings/strings/strings_stringbuilder/string_05.java
@@ -14,19 +14,22 @@
 package strings_stringbuilder_arraylist.strings.strings.strings_stringbuilder;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class string_05 {
     public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
         System.out.println("Enter Input :-");
-
+        
 		int n = scn.nextInt();
-
+        
 		ArrayList<Integer> al = new ArrayList<>();
 		for(int i = 0 ; i < n; i++){
-			al.add(scn.nextInt());
+            al.add(scn.nextInt());
 		}
+
+        System.out.println("Output :-");
 		solution(al);
 		System.out.println(al);
 
@@ -52,3 +55,5 @@ public class string_05 {
 		
 	}
 }
+
+
