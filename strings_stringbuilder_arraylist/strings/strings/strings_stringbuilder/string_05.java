56673610594e91daf432f0cdd293bb6496d2fac4
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-30 11:22:09 +0530

    solved string-5 question

--- strings_stringbuilder_arraylist/strings/strings/strings_stringbuilder/string_05.java
+++ strings_stringbuilder_arraylist/strings/strings/strings_stringbuilder/string_05.java
@@ -13,31 +13,29 @@
 
 package strings_stringbuilder_arraylist.strings.strings.strings_stringbuilder;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class string_05 {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter input :-");
+		Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Input :-");
 
-        int n = scn.nextInt();
-        
-        ArrayList<Integer> al = new ArrayList<>();
-        for(int i = 0 ; i < n; i++){
-            al.add(scn.nextInt());
-        }
-        
-        solution(al);
-        System.out.println("Output :-");
-        System.out.println(al);
+		int n = scn.nextInt();
+
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++){
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
 
         scn.close();
-    }
+	}
     
     public static boolean isPrime(int val) {
-        for(int div = 2 ; div * div <= val ; div++) {
-            if(val % div == 0) {
+        for(int div = 2; div*div <= val; div++) {
+            if(val%div==0) {
                 return false;
             }
         }
@@ -45,12 +43,12 @@ public class string_05 {
     }
 
     public static void solution(ArrayList<Integer> al){
-        for(int idx = al.size()-1 ; idx >= 0 ; idx--) {
-            int val = al.get(idx);
-
-            if(isPrime(val)) {
-                al.remove(idx);
-            }
-        }
-    }
+		for(int idx = al.size()-1; idx >= 0; idx--) {
+		    int val = al.get(idx);
+		    if(isPrime(val)) {
+		        al.remove(idx);
+		    }
+		}
+		
+	}
 }
