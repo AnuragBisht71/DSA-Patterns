f5395dd56b94552b9e988b65a03bf5b7dbcb15db
AnuragBisht71 <bishtanurag71@gmail.com>
2023-05-08 12:34:19 +0530

    solved recursion on the way up-4 question

--- recursion/recursionOnTheWayUp/recursion_04.java
+++ recursion/recursionOnTheWayUp/recursion_04.java
@@ -35,17 +35,16 @@ public class recursion_04 {
     // dr - destination row
     // dc - destination column
     public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if(sr > dr || sc > dc) {
+            return;
+        }
+
         if(sr == dr && sc == dc) {
             System.out.println(psf);
             return;
         }
 
-        if(sr <= dr) {
-            printMazePaths(sr+1, sc, dr, dc, psf + "h");
-        }
-
-        if(sc <= dc) {
-            printMazePaths(sr, sc+1, dr, dc, psf + "v");
-        }
+        printMazePaths(sr+1, sc, dr, dc, psf + "h");
+        printMazePaths(sr, sc+1, dr, dc, psf + "v");
     }
 }
