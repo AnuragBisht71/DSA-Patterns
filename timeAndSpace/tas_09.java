116d7299170057878e44f1e884c8801209e4607a
AnuragBisht71 <bishtanurag71@gmail.com>
2023-05-23 07:59:05 +0530

    solved time and space question-9

--- timeAndSpace/tas_09.java
+++ timeAndSpace/tas_09.java
@@ -1,23 +1,31 @@
+// Sort 01
+
+// Input :-
+
+// Output :-
+
+
+
 package timeAndSpace;
 
 public class tas_09 {
     int i = 0 , j = 0 , k = arr.length - 1;
 
     while(i <= k) {
-        if(arr[i] == 1) {
+        if(arr[i] == 0) {
             swap(arr,i,j);
             i++;
             j++;
         } 
 
+        else if(arr[i] == 1) {
+            i++;
+        }
+
         else if(arr[i] == 2) {
             swap(arr,i,k);
-            i++;
             k--;
         }
 
-        else {
-            i++;
-        }
     }
 }
