aa3cd31257c64d5e258c17408f6685f582aa2e48
AnuragBisht71 <bishtanurag71@gmail.com>
2023-05-24 17:37:18 +0530

    solved time and space question-11

--- timeAndSpace/tas_11.java
+++ timeAndSpace/tas_11.java
@@ -17,19 +17,82 @@
 // 4
 // 7
 
-
-
 package timeAndSpace;
 
+import java.util.Scanner;
+
 public class tas_11 {
-    if(lo == hi) {
-        return new int[]{arr[lo]};
+    public static int[] mergeSort(int[] arr, int lo, int hi) {
+        if (lo == hi) {
+            return new int[] { arr[lo] };
+        }
+
+        int mid = (lo + hi) / 2;
+
+        int lp[] = mergeSort(arr, lo, mid);
+        int rp[] = mergeSort(arr, mid + 1, hi);
+
+        return mergeTwoSortedArrays(lp, rp);
     }
 
-    int mid = (lo + hi) / 2;
+    // used for merging two sorted arrays
+    public static int[] mergeTwoSortedArrays(int[] a, int[] b) {
+        System.out.println("Merging these two arrays ");
+        System.out.print("left array -> ");
+        print(a);
+        System.out.print("right array -> ");
+        print(b);
+        int i = 0, j = 0, k = 0;
+        int[] ans = new int[a.length + b.length];
+        while (i < a.length && j < b.length) {
+            if (a[i] <= b[j]) {
+                ans[k] = a[i];
+                i++;
+                k++;
+            } else {
+                ans[k] = b[j];
+                j++;
+                k++;
+            }
+        }
 
-    int lp[] = mergeSort(arr,lo,mid);
-    int rp[] = mergeSort(arr,mid+1,hi);
+        while (i < a.length) {
+            ans[k] = a[i];
+            k++;
+            i++;
+        }
 
-    return mergeTwoSortedArrays(lp , rp);
+        while (j < b.length) {
+            ans[k] = b[j];
+            k++;
+            j++;
+        }
+
+        return ans;
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Input :-");
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int[] sa = mergeSort(arr, 0, arr.length - 1);
+        System.out.print("Sorted Array -> ");
+        
+        System.out.println("Output :-");
+        print(sa);
+
+        scn.close();
+    }
 }
