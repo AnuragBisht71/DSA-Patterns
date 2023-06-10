ded320610d7edb7b2477ee3b358b7f1039d6dc13
AnuragBisht71 <bishtanurag71@gmail.com>
2023-06-10 12:35:08 +0530

    solved dynamic programming question

--- dynamicProgramming/count_sebsequences.java
+++ dynamicProgramming/count_sebsequences.java
@@ -8,32 +8,34 @@
 
 // 7
 
-
-
 package dynamicProgramming;
 
 import java.util.Scanner;
 
 public class count_sebsequences {
-    Scanner scn = new Scanner(System.in);
-    String str = scn.nextLine();
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
 
-    int a = 0 , ab = 0 , abc = 0;
+        int a = 0;
+        int ab = 0;
+        int abc = 0;
 
-    for(int i = 0 ; i < str.length() ; i++) {
-        char ch = str.charAt(i);
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
 
-        if(ch == 'a') {
-            a = 2 * a + 1;
+            if (ch == 'a') {
+                a = 2 * a + 1;
+            } else if (ch == 'b') {
+                ab = 2 * ab + a;
+            } else if (ch == 'c') {
+                abc = 2 * abc + ab;
+            }
         }
-        else if(ch == 'b') {
-            ab = 2 + ab + a;
-        }
-        else if(ch == 'c') {
-            abc = 2 * abc + ab;
-        }
+
+        System.out.println("Output :-");
+        System.out.println(abc);
+
+        scn.close();
     }
-
-    System.out.println("Output :-");
-    System.out.println(abc);
 }
