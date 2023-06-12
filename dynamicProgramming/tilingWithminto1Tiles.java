665cc3df279f6ded54a2faa4bac65f36b0971f1d
AnuragBisht71 <bishtanurag71@gmail.com>
2023-06-12 06:26:29 +0530

    solved dynamic programming questions

---
+++ dynamicProgramming/tilingWithminto1Tiles.java
@@ -0,0 +1,42 @@
+// Input :-
+
+// 39 --> n
+// 16 --> m
+
+// Output :-
+
+// 61
+
+
+
+package dynamicProgramming;
+
+import java.util.Scanner;
+
+public class tilingWithminto1Tiles {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Input :-");
+
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int dp[] = new int[n+1];
+        for(int i = 0 ; i <= n ; i++) {
+            if(i < m) {
+                dp[i] = 1;
+            }
+            else if(i == m) {
+                dp[i] = 2;
+            }
+            else {
+                dp[i] = dp[i-1] + dp[i-m];
+            }
+        }
+
+        System.out.println("Output :-");
+        System.out.println(dp[n]);
+
+        scn.close();
+    }
+}
