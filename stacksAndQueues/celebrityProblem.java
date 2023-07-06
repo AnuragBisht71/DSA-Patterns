780600e5a4ad37eb30555f99a1a5cb68844327e1
AnuragBisht71 <bishtanurag71@gmail.com>
2023-07-06 13:21:29 +0530

    solved celebrity problem of stacks and queues

---
+++ stacksAndQueues/celebrityProblem.java
@@ -0,0 +1,71 @@
+// Input :-
+
+// 4
+// 0000
+// 1011
+// 1101
+// 1110
+
+// Output :-
+
+// 0
+
+
+
+package stacksAndQueues;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class celebrityProblem {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Input :-");
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int j = 0; j < n; j++) {
+            String line = br.readLine();
+            for (int k = 0; k < n; k++) {
+                arr[j][k] = line.charAt(k) - '0';
+            }
+        }
+
+        System.out.println("Output :-");
+        findCelebrity(arr);
+
+    }
+
+    public static void findCelebrity(int[][] arr) {
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i < arr.length ; i++) {
+            st.push(i);
+        }
+
+        while(st.size() >= 2) {
+            int i = st.pop();
+            int j = st.pop();
+
+            if(arr[i][j] == 1) {
+                st.push(j);
+            }
+            else {
+                st.push(i);
+            }
+        }
+
+        int pot = st.pop();
+        for(int i = 0 ; i < arr.length ; i++) {
+            if(i != pot) {
+                if(arr[i][pot] == 0 || arr[pot][i] == 1) {
+                    System.out.println("none");
+                    return;
+                }
+            }
+        }
+
+        System.out.println(pot);
+    }
+}
