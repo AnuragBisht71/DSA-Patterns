6a8aeac90b0dd0018ff33dd69717d2ebfb39d888
AnuragBisht71 <bishtanurag71@gmail.com>
2023-07-17 17:06:30 +0530

    question format

--- stacksAndQueues/largestAreaHistogram.java
+++ stacksAndQueues/largestAreaHistogram.java
@@ -13,10 +13,20 @@
 
 // 12
 
-
-
 package stacksAndQueues;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class largestAreaHistogram {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Input :-");
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+    }
 }
